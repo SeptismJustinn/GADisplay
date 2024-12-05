0b6052bfd0f621957760b1caf7a76b475ca0e7cf
JustinnTanCK <81621295+SeptismJustinn@users.noreply.github.com>
2024-12-05 15:54:54 +0800

    Fixed time string construction

--- front/src/pages/components/WeekalendarRow.jsx
+++ front/src/pages/components/WeekalendarRow.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import styles from "./css/WeekalendarRow.module.css";
 
 function WeekalendarRow(props) {
+  function getTimeString(date) {
+    const meridiem = date.getHours() > 11 ? "PM" : "AM"
+    return `${date.getHours()}:${String(date.getMinutes()).padStart(2, "0")} ${meridiem}`
+  }
   function extractNames(col) {
     // Check if multiples events conflict
     const output = col.map((item, idx) => {
-      const startArr = item.starts.toLocaleTimeString().split(" ");
-      const endArr = item.ends.toLocaleTimeString().split(" ");
       // If adhoc, show purpose in dashboard instead of event name
       // Different from display
       return (
         <div key={idx}>
           {item.schedule ? item.name : item.purpose}
           <br />
-          {`(${startArr[0].slice(0, -3) + startArr[1]}-${
-            endArr[0].slice(0, -3) + endArr[1]
-          })`}
+          {`(${getTimeString(item.starts)}-${getTimeString(item.ends)})`}
         </div>
       );
     });
@@ -27,51 +27,44 @@ function WeekalendarRow(props) {
     <div className={props.id % 2 == 0 ? styles.alt_row : styles.row}>
       <div className={styles.room}>{props.id}</div>
       <div
-        className={`${props.sunday === 0 ? styles.sunday : ""} ${
-          props.columns[0].length > 1 ? styles.overload_text : ""
-        }`}
+        className={`${props.sunday === 0 ? styles.sunday : ""} ${props.columns[0].length > 1 ? styles.overload_text : ""
+          }`}
       >
         {extractNames(props.columns[0])}
       </div>
       <div
-        className={`${props.sunday === 1 ? styles.sunday : ""} ${
-          props.columns[1].length > 1 ? styles.overload_text : ""
-        }`}
+        className={`${props.sunday === 1 ? styles.sunday : ""} ${props.columns[1].length > 1 ? styles.overload_text : ""
+          }`}
       >
         {extractNames(props.columns[1])}
       </div>
       <div
-        className={`${props.sunday === 2 ? styles.sunday : ""} ${
-          props.columns[2].length > 1 ? styles.overload_text : ""
-        }`}
+        className={`${props.sunday === 2 ? styles.sunday : ""} ${props.columns[2].length > 1 ? styles.overload_text : ""
+          }`}
       >
         {extractNames(props.columns[2])}
       </div>
       <div
-        className={`${props.sunday === 3 ? styles.sunday : ""} ${
-          props.columns[3].length > 1 ? styles.overload_text : ""
-        }`}
+        className={`${props.sunday === 3 ? styles.sunday : ""} ${props.columns[3].length > 1 ? styles.overload_text : ""
+          }`}
       >
         {extractNames(props.columns[3])}
       </div>
       <div
-        className={`${props.sunday === 4 ? styles.sunday : ""} ${
-          props.columns[4].length > 1 ? styles.overload_text : ""
-        }`}
+        className={`${props.sunday === 4 ? styles.sunday : ""} ${props.columns[4].length > 1 ? styles.overload_text : ""
+          }`}
       >
         {extractNames(props.columns[4])}
       </div>
       <div
-        className={`${props.sunday === 5 ? styles.sunday : ""} ${
-          props.columns[5].length > 1 ? styles.overload_text : ""
-        }`}
+        className={`${props.sunday === 5 ? styles.sunday : ""} ${props.columns[5].length > 1 ? styles.overload_text : ""
+          }`}
       >
         {extractNames(props.columns[5])}
       </div>
       <div
-        className={`${props.sunday === 6 ? styles.sunday : ""} ${
-          props.columns[6].length > 1 ? styles.overload_text : ""
-        }`}
+        className={`${props.sunday === 6 ? styles.sunday : ""} ${props.columns[6].length > 1 ? styles.overload_text : ""
+          }`}
       >
         {extractNames(props.columns[6])}
       </div>
