999a6e84b09b966d41f00d60e3d3e0006fe7c3e4
JustinnTanCK <81621295+SeptismJustinn@users.noreply.github.com>
2024-12-05 15:55:07 +0800

    Added "Back to login" button for error page

--- front/src/pages/AdminErrorPage.jsx
+++ front/src/pages/AdminErrorPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function AdminErrorPage() {
   return (
     <div style={{ margin: "5vh 5%" }}>
       <h4>Please contact an Admin and wait for them to grant you access.</h4>
+      <Link to="/admin">
+        <button>Back to Login</button>
+      </Link>
     </div>
   );
 }
